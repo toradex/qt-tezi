toradex/qt-tezi

commit a5666856781ac793186d3dd5158bdd1d80cfe02f

mainwindow: change license dialog ok button-text to i accept
since this is also written like that in the eula license

Signed-off-by: Philippe Schenker <philippe.schenker@toradex.com>

mainwindow.cpp
@@ -1289,7 +1289,7 @@ void MainWindow::startImageWrite(QVariantMap &entry)
     if (entry.contains("license") && !(_isAutoinstall || _acceptAllLicenses)) {
         QByteArray text = getFileContents(folder + "/" + entry.value("license").toString());
         ScrollTextDialog eula(entry.value("license_title").toString(), QString(text), QDialogButtonBox::Yes | QDialogButtonBox::Abort);
-        eula.setButtonText(QDialogButtonBox::Yes, tr("I A&gree"));
+        eula.setButtonText(QDialogButtonBox::Yes, tr("I A&ccept"));
         eula.setButtonShortcut(QDialogButtonBox::Yes, QKeySequence("g"));
         eula.setButtonText(QDialogButtonBox::Abort, tr("A&bort"));
         eula.setButtonShortcut(QDialogButtonBox::Abort, QKeySequence("b"));

commit ec1a117d50f6c69fea4131cc8db7be735292992b

mainwindow: fix shortcut on license dialog
Fixes: commit a566685 ("mainwindow: change license dialog ok button-text to i accept")
Signed-off-by: Philippe Schenker <philippe.schenker@toradex.com>

mainwindow.cpp
@@ -1290,7 +1290,7 @@ void MainWindow::startImageWrite(QVariantMap &entry)
         QByteArray text = getFileContents(folder + "/" + entry.value("license").toString());
         ScrollTextDialog eula(entry.value("license_title").toString(), QString(text), QDialogButtonBox::Yes | QDialogButtonBox::Abort);
         eula.setButtonText(QDialogButtonBox::Yes, tr("I A&ccept"));
-        eula.setButtonShortcut(QDialogButtonBox::Yes, QKeySequence("g"));
+        eula.setButtonShortcut(QDialogButtonBox::Yes, QKeySequence("c"));
         eula.setButtonText(QDialogButtonBox::Abort, tr("A&bort"));
         eula.setButtonShortcut(QDialogButtonBox::Abort, QKeySequence("b"));
         eula.setDefaultButton(QDialogButtonBox::Abort);