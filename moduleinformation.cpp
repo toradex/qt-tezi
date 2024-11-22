d5b38fca080185b1861be35004e6bd6a5a5b9353
Stefan Eichenberger <stefan.eichenberger@toradex.com>
2024-11-22 15:29:11 +0100

    moduleinformation: Add new Apalis iMX8 PIDs
    Add support for the new Apalis iMX8 PIDs. This are the modules with the
    new 1300MHz (IT instead of automotive) version of the SoC.
    
    Related-to: TEI-1212
    Signed-off-by: Stefan Eichenberger <stefan.eichenberger@toradex.com>

--- moduleinformation.cpp
+++ moduleinformation.cpp
@@ -181,7 +181,7 @@ ModuleInformation *ModuleInformation::detectModule(QObject *parent)
             }
         } else if (socid == "i.MX8QM") {
             // i.MX 8QuadMax/QuadPlus
-            productIds << 37 << 47 << 48 << 49 << 67;
+            productIds << 37 << 47 << 48 << 49 << 67 << 91 << 92 << 93 << 94 << 95;
             storageClass = StorageClass::Block;
             rebootWorks = false;
         } else if (socid == "i.MX8QXP") {
