toradex/qt-tezi

commit 561041c64a9cff6461b8b6ea1fa40698f8a9edfb

configblock: Add support to PID4 0090
Add PID4 0090 support for Verdin iMX8M Mini Quad 4GB WB ET.

Additionally, add PID 0089 as a placeholder for
Verdin iMX95 Hexa 16GB WB IT, which will change once this hardware
gets support.

Related-to: TEI-1200

Signed-off-by: Vitor Soares <vitor.soares@toradex.com>

configblock.cpp
@@ -100,6 +100,8 @@ const char* const toradex_modules[] = {
 	[86] = "0086 Verdin iMX8M Mini DualLite 2GB IT",
 	[87] = "0087 Verdin iMX8M Mini Quad 2GB IT",
 	[88] = "0088 Aquila AM69 Octa 32GB WB IT",
+	[89] = "0089 UNKNOWN MODULE",
+	[90] = "0090 Verdin iMX8M Mini Quad 4GB WB ET",
 };
 
 const char * const toradex_prototype_modules[] = {

commit 0cb434fbb016b902dc36079b2c6354b1c8a72ca1

configblock: correct/remove unknown module entries
Correct the known module names marked unknown, and remove the ones
unknown. These automatically fall back to index 0 of the list.

Related-to: TEI-1227
Fixes: 561041c64a9c ("configblock: Add support to PID4 0090")
Signed-off-by: Vitor Soares <vitor.soares@toradex.com>

configblock.cpp
@@ -29,8 +29,6 @@ const struct toradex_som toradex_modules[] = {
     {15, "0015 Colibri iMX6DL 512MB"},
     {16, "0016 Colibri iMX6S 256MB IT"},
     {17, "0017 Colibri iMX6DL 512MB IT"},
-    {18, "0018 UNKNOWN MODULE"},
-    {19, "0019 UNKNOWN MODULE"},
     {20, "0020 Colibri T20 256MB"},
     {21, "0021 Colibri T20 512MB"},
     {22, "0022 Colibri T20 512MB IT"},
@@ -100,7 +98,7 @@ const struct toradex_som toradex_modules[] = {
     {86, "0086 Verdin iMX8M Mini DualLite 2GB IT"},
     {87, "0087 Verdin iMX8M Mini Quad 2GB IT"},
     {88, "0088 Aquila AM69 Octa 32GB WB IT"},
-    {89, "0089 UNKNOWN MODULE"},
+    {89, "0089 Verdin iMX95 Hexa 16GB WB IT"},
     {90, "0090 Verdin iMX8M Mini Quad 4GB WB ET"},
     {91, "0091 Apalis iMX8QM 4GB WB IT"},
     {92, "0092 Apalis iMX8QM 4GB IT"},