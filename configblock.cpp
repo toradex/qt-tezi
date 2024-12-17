toradex/qt-tezi

commit 774db3c0dfc441f9e04d9d55f870374e7e04acf4

configblock: add 0096 SMARC iMX95 and 0098 Aquila iMX95
Add the string representation of PID 0096 and 0098, upcoming SMARC iMX95
Hexa 8GB WB IT and Aquila iMX95 Hexa 16GB WB IT.

Related-to: TEI-1221
Signed-off-by: Hiago De Franco <hiago.franco@toradex.com>

configblock.cpp
@@ -105,6 +105,8 @@ const struct toradex_som toradex_modules[] = {
     {93, "0093 Apalis iMX8QP 2GB WB"},
     {94, "0094 Apalis iMX8QP 2GB"},
     {95, "0095 Apalis iMX8QM 8GB WB IT"},
+    {96, "0096 Toradex Smarc iMX95 Hexa 8GB WB IT"},
+    {98, "0098 Aquila iMX95 Hexa 16GB WB IT"},
     {2600, "2600 Apalis iMX8QXP 2GB ECC WB IT PROTO"},
 };
 

commit 43c8a1f4f6f4af004e5a843bae93a18d2d7b436d

configblock.cpp: fix lowercase SMARC naming
Fix wrong "Smarc" lowercase naming.

Fixes: 774db3c0dfc4 ("configblock: add 0096 SMARC iMX95 and 0098 Aquila iMX95")
Signed-off-by: Hiago De Franco <hiago.franco@toradex.com>

configblock.cpp
@@ -105,7 +105,7 @@ const struct toradex_som toradex_modules[] = {
     {93, "0093 Apalis iMX8QP 2GB WB"},
     {94, "0094 Apalis iMX8QP 2GB"},
     {95, "0095 Apalis iMX8QM 8GB WB IT"},
-    {96, "0096 Toradex Smarc iMX95 Hexa 8GB WB IT"},
+    {96, "0096 Toradex SMARC iMX95 Hexa 8GB WB IT"},
     {97, "0097 Toradex SMARC iMX8M Plus Quad 4GB WB IT"},
     {98, "0098 Aquila iMX95 Hexa 16GB WB IT"},
     {2600, "2600 Apalis iMX8QXP 2GB ECC WB IT PROTO"},