toradex/qt-tezi

commit 87dbe839422e02e5ff129e6991fe79f8dcf0c0df

mainwindow: wait for UDC to come up to initialize USB gadget
The UDC (USB Device Controller) may not be immediately available during
the initialization of the USB gadget. Specifically, NCM will not work in
this case as that gadget config is enabled in the main window init.

This happens when libusbgx is initialized before the UDC is ready. It
will not find UDCs added later as this is an atypical situation.

Poll for the UDC to appear before initializing any USB gadgets. This is
done with a new separate thread, UdcPollThread, so no other parts of the
application are slowed down.

Related-to: TEI-1300
Signed-off-by: Ernest Van Hoecke <ernest.vanhoecke@toradex.com>

mainwindow.cpp
@@ -110,10 +110,33 @@ bool MainWindow::setAvahiHostname(const QString hostname) {
     return false;
 }
 
+void MainWindow::initUsbGadget() {
+    _usbGadget->initialize();
+
+    if (_moduleInformation->storageClass() == ModuleInformation::StorageClass::Block) {
+        _usbGadget->setMassStorageDev(_moduleInformation->mainPartition());
+        if (_usbGadget->initMassStorage() &&
+            _usbGadget->setMassStorageAttrs(_serialNumber, _toradexProductName, _toradexProductId))
+            ui->actionUsbMassStorage->setEnabled(true);
+    }
+
+    if (_usbGadget->initNcm() &&
+        _usbGadget->setNcmAttrs(_serialNumber, _toradexProductName, _toradexProductId)) {
+        ui->actionUsbNcm->setEnabled(true);
+        ui->actionUsbNcm->trigger();
+    }
+}
+
 bool MainWindow::initialize() {
     _usbGadget = new UsbGadget();
-    _usbGadget->initNcm();
-    _usbGadget->enableNcm(true);
+
+    /* Disable NCM and Storage actions by default, enable if relevant in initUsbGadget */
+    ui->actionUsbNcm->setEnabled(false);
+
+    if (_moduleInformation->storageClass() != ModuleInformation::StorageClass::Block)
+        ui->mainToolBar->removeAction(ui->actionUsbMassStorage);
+    else
+        ui->actionUsbMassStorage->setEnabled(false);
 
     _moduleInformation = ModuleInformation::detectModule(this);
     if (_moduleInformation == nullptr) {
@@ -192,30 +215,11 @@ bool MainWindow::initialize() {
     _internetIcon = QIcon(":/icons/download_cloud.png");
 
     _mediaPollThread = new MediaPollThread(_moduleInformation, this);
+    _udcPollThread = new UdcPollThread(this);
 
     ui->list->setSelectionMode(QAbstractItemView::SingleSelection);
 
     _availableMB = static_cast<int>(_moduleInformation->getStorageSize() / (1024 * 1024));
-
-    if (_moduleInformation->storageClass() == ModuleInformation::StorageClass::Block) {
-        _usbGadget->setMassStorageDev(_moduleInformation->mainPartition());
-        if (_usbGadget->initMassStorage() &&
-            _usbGadget->setMassStorageAttrs(_serialNumber, _toradexProductName, _toradexProductId))
-            ui->actionUsbMassStorage->setEnabled(true);
-        else
-            ui->actionUsbMassStorage->setEnabled(false);
-    } else {
-        ui->mainToolBar->removeAction(ui->actionUsbMassStorage);
-    }
-
-    if (_usbGadget->initNcm() &&
-        _usbGadget->setNcmAttrs(_serialNumber, _toradexProductName, _toradexProductId)) {
-        ui->actionUsbNcm->setEnabled(true);
-        ui->actionUsbNcm->trigger();
-
-    } else {
-        ui->actionUsbNcm->setEnabled(false);
-    }
 
     // Add static server list
     FeedServer srv;
@@ -258,10 +262,13 @@ bool MainWindow::initialize() {
     connect(_browser, SIGNAL (serviceEntryAdded(QString)), this, SLOT (addService(QString)));
     connect(_browser, SIGNAL (serviceEntryRemoved(QString)), this, SLOT (removeService(QString)));
     connect(_httpApi, SIGNAL (newImageUrl(const QString)), this, SLOT (addNewImageUrl(const QString)));
+    // Initialize USB gadget once a USB Device Controller is available
+    connect(_udcPollThread, SIGNAL (finished()), this, SLOT (initUsbGadget()));
 
     _browser->browse("_tezi._tcp");
     _httpApi->start("8080",this);
     _mediaPollThread->start();
+    _udcPollThread->start();
 
     return true;
 }

mainwindow.h
@@ -21,6 +21,7 @@
 #include "feedserver.h"
 #include "imagelist.h"
 #include "httpapi.h"
+#include "udcpollthread.h"
 #include <QMainWindow>
 #include <QModelIndex>
 #include <QMessageBox>
@@ -113,6 +114,7 @@ protected:
     QVariantMap _imageEntry;
     UsbGadget *_usbGadget;
     MediaPollThread *_mediaPollThread;
+    UdcPollThread *_udcPollThread;
     MultiImageWriteThread *_imageWriteThread;
     int _internetHostLookupId;
     ZConfServiceBrowser *_browser;
@@ -161,6 +163,7 @@ protected slots:
     void errorMounting(const QString blockdev);
     void disableFeed(const QString feedname);
     void imageHostLookupResults(QHostInfo hostInfo);
+    void initUsbGadget();
 
     /* Events from ImageListDownload */
     void onImageListDownloadFinished();

tezi.pro
@@ -53,7 +53,8 @@ SOURCES += main.cpp\
     imagelistdownload.cpp \
     feedsdialog.cpp \
     waitingspinnerwidget.cpp \
-    qlistimagewidgetitem.cpp
+    qlistimagewidgetitem.cpp \
+    udcpollthread.cpp
 
 HEADERS  += mainwindow.h \
     httpapi.h \
@@ -94,7 +95,8 @@ HEADERS  += mainwindow.h \
     feedserver.h \
     imagesource.h \
     waitingspinnerwidget.h \
-    qlistimagewidgetitem.h
+    qlistimagewidgetitem.h \
+    udcpollthread.h
 
 FORMS    += mainwindow.ui \
     languagedialog.ui \

udcpollthread.cpp
@@ -1,0 +1,36 @@
+#include "udcpollthread.h"
+#include <chrono>
+#include <QDebug>
+#include <QDir>
+
+/*
+ * Polls until a UDC (USB Device Controller) is available in sysfs.
+ *
+ * This is needed because the UDC driver may take some time to load
+ * and when initializing, libusbgx will assume that the default UDC
+ * is already present. Delaying all relevant initialization until
+ * a UDC is found avoids complexity.
+ */
+UdcPollThread::UdcPollThread(QObject *parent) : QThread(parent)
+{
+}
+
+void UdcPollThread::run() {
+    QDir dir ("/sys/class/udc", "*", QDir::Name, QDir::Dirs | QDir::NoDotAndDotDot);
+
+    auto start = std::chrono::steady_clock::now();
+
+    qDebug() << "Scanning for UDC...";
+
+    while (dir.count() == 0) {
+        msleep(50);
+        dir.refresh();
+    }
+
+    auto end = std::chrono::steady_clock::now();
+    auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+    qDebug() << "Waited " << elapsed_ms << " ms for the first UDC entry.";
+
+    emit finished();
+}

udcpollthread.h
@@ -1,0 +1,20 @@
+#ifndef UDCPOLLTHREAD_H
+#define UDCPOLLTHREAD_H
+
+#include <QObject>
+#include <QThread>
+
+class UdcPollThread : public QThread
+{
+    Q_OBJECT
+
+public:
+    UdcPollThread(QObject *parent);
+
+    void run();
+
+signals:
+    void finished();
+};
+
+#endif // UDCPOLLTHREAD_H

usbgadget.cpp
@@ -10,6 +10,10 @@
 UsbGadget::UsbGadget(QObject *parent) : QObject(parent),
     _gadgetInitialized(false), _gadgetIsMassStorage(false)
 {
+}
+
+void UsbGadget::initialize()
+{
     if (usbgadget_init()) {
         qDebug() << "USB Gadget: Error initalizing:" << usbgadget_strerror();
         return;

usbgadget.h
@@ -8,6 +8,7 @@ class UsbGadget : public QObject
     Q_OBJECT
 public:
     explicit UsbGadget(QObject *parent = 0);
+    void initialize();
     bool initMassStorage();
     void enableMassStorage(bool enable);
     bool isMassStorageSafeToRemove();

commit b92f78f57b9b228f696b99b9118dd089b49e3a3d

mainwindow: fix use of _moduleInformation before initialization
The new USB gadget initialization flow introduced a bug by accessing
_moduleInformation->moduleSupported() before _moduleInformation was
initialized. On BSP7/Scarthgap this ran correctly by chance, but of
course the logic is wrong and the invalid access can also lead to
crashes, which is the case for BSP6/Kirkstone.

Fix this by respecting the correct order of operations.

Related-to: TEI-1300
Fixes: 87dbe839422e ("mainwindow: wait for UDC to come up to initialize USB gadget")
Signed-off-by: Ernest Van Hoecke <ernest.vanhoecke@toradex.com>

mainwindow.cpp
@@ -133,11 +133,6 @@ bool MainWindow::initialize() {
     /* Disable NCM and Storage actions by default, enable if relevant in initUsbGadget */
     ui->actionUsbNcm->setEnabled(false);
 
-    if (_moduleInformation->storageClass() != ModuleInformation::StorageClass::Block)
-        ui->mainToolBar->removeAction(ui->actionUsbMassStorage);
-    else
-        ui->actionUsbMassStorage->setEnabled(false);
-
     _moduleInformation = ModuleInformation::detectModule(this);
     if (_moduleInformation == nullptr) {
         QMessageBox::critical(nullptr, QObject::tr("Module Detection failed"),
@@ -152,6 +147,11 @@ bool MainWindow::initialize() {
                               QMessageBox::Close);
         return false;
     }
+
+    if (_moduleInformation->storageClass() != ModuleInformation::StorageClass::Block)
+        ui->mainToolBar->removeAction(ui->actionUsbMassStorage);
+    else
+        ui->actionUsbMassStorage->setEnabled(false);
 
 #ifdef __x86_64__
     _toradexConfigBlock = ConfigBlock::configBlockFromUserInput(27, "V1.1A", "01234567");